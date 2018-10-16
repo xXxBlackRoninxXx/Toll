3383416369585fa3b16266ce261b2095d590fad6
roma_ <s126511erp@rb.asu.ru>
2018-10-16 13:39:49 +0300

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-toolbar>
+    <v-toolbar-side-icon></v-toolbar-side-icon>
+    <v-toolbar-title>Title</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-toolbar-items class="hidden-sm-and-down">
+      <v-btn flat>
+        <v-icon dark>build</v-icon>
+      </v-btn>
+      <v-btn flat>g</v-btn>
+      <v-btn color="blue" class="text--white">Logine</v-btn>
+    </v-toolbar-items>
+  </v-toolbar>
+</template>
